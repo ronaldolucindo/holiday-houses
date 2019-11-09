79cb0f18e81232bfabc69b8a742e3376d031eaa1
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2019-11-09 13:24:19 -0300

    Add  search page initial structure

--- src/pages/search-houses/index.js
+++ src/pages/search-houses/index.js
@@ -1,15 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { searchHousesRequest } from 'actions/houses';
+import Typography from '@material-ui/core/Typography';
+import { searchHouses } from 'actions/search';
+import SearchForm from 'components/search-form';
+
+import './styles.css';
 
 function SearchHouses(props) {
-  props.searchHousesRequest('florianopolis');
-  return <p>SearchHouses</p>;
+  const { searchTerm, searchHouses, history } = props;
+
+  const handleInputChange = e => {
+    searchHouses(e.target.value);
+  };
+
+  const handleSearchSubmit = e => {
+    e.preventDefault();
+    const searchParam = encodeURI(searchTerm);
+    history.push(`/search/${searchParam}`);
+  };
+
+  return (
+    <main className="search-houses-container">
+      <Typography variant="h2" component="h1" gutterBottom>
+        your next holiday house
+      </Typography>
+      <SearchForm
+        value={searchTerm}
+        onChange={handleInputChange}
+        onSubmit={handleSearchSubmit}
+      />
+    </main>
+  );
 }
 
+const mapStateToProps = state => ({
+  searchTerm: state.search.searchTerm
+});
+
+const mapDipatchToProps = {
+  searchHouses
+};
+
+SearchHouses.propType = {
+  searchTerm: PropTypes.string,
+  searchHouses: PropTypes.func,
+  history: PropTypes.object
+};
+
 export default connect(
-  null,
-  {
-    searchHousesRequest
-  }
+  mapStateToProps,
+  mapDipatchToProps
 )(SearchHouses);
